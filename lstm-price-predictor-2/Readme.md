a448debdbfcdee50d1b34e466a574e40caa368cd
Danh Nguyen2 <Danh_Nguyen2@edwards.com>
2024-06-28 15:21:01 -0700

    - added a price predictor for nike that will predict the price of nike for tomorrow based only last 4 years of time price series

---
+++ lstm-price-predictor-2/Readme.md
@@ -0,0 +1,3 @@
+## Description
+
+The purpose of this program will be to read the price of Nike and laggingly predict the price of it for the next day using the last 60 days. 
--- lstm-price-predictor-2/main.py
+++ lstm-price-predictor-2/main.py
@@ -2,10 +2,13 @@ import os
 import numpy as np
 import pandas as pd
 import yfinance as yf
+import math
 import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import MinMaxScaler
 from datetime import datetime
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout
 
 dataFileName = 'nikePriceData.csv'
 todayDT = datetime.now().strftime('%Y-%m-%d')
@@ -22,8 +25,71 @@ else:
     except Exception as e:
         print(f"An error occurred: {e}")
 
-
+nikeDataDF.set_index('Date', inplace=True)
+print(nikeDataDF)
 trainingData = nikeDataDF.iloc[:, 1:2].values #trainingData before this line is a dataframe, we need to get only open column and turn it into a numpy array to do numerical operation easier
 
 scaler = MinMaxScaler(feature_range = (0,1)) #create a minmaxscaler and set bound
-scaled_training_set = scaler.fit_transform(trainingData) #feature scaled the 2nd column
+scaledTrainingData = scaler.fit_transform(trainingData) #feature scaled the 2nd column
+trainingDataSize = math.ceil(.8 * scaledTrainingData.size)
+#-------- Lets prep x_train, y_train, x_test, y_test --------
+x_train, y_train, x_test, y_test = [], [], [], []
+
+for i in range(60, trainingDataSize):
+    x_train.append(scaledTrainingData[i-60:i, 0])
+    y_train.append(scaledTrainingData[i, 0])
+
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+
+# LSTM need 3 dimensional input (number of samples, number of time steps,  number of features)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+
+regressorModel = Sequential()
+regressorModel.add(LSTM(units = 50, return_sequences=True, input_shape=(x_train.shape[1], 1))) #return_sequences attribute means the LSTM layer will have an output of 
+regressorModel.add(Dropout(0.2))
+
+regressorModel.add(LSTM(units = 50, return_sequences=True))
+regressorModel.add(Dropout(0.2))
+
+regressorModel.add(LSTM(units = 50, return_sequences=True))
+regressorModel.add(Dropout(0.2))
+
+regressorModel.add(LSTM(units = 50))
+regressorModel.add(Dropout(0.2))
+
+regressorModel.add(Dense(units=1)) #the model last layer will be a densely connected means all output from previous layer will be input to this layer
+
+regressorModel.compile(optimizer='adam', loss='mean_squared_error')
+regressorModel.fit(x_train, y_train, epochs=20, batch_size=32)
+
+testingData = scaledTrainingData[trainingDataSize-60:, :]
+y_test = scaledTrainingData[trainingDataSize:, :]
+
+for i in range(60, len(testingData)):
+    x_test.append(testingData[i-60:i, 0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+# ******** Lets predict the price of stock ******** #
+predictedStockPrice = regressorModel.predict(x_test)
+predictedStockPrice = scaler.inverse_transform(predictedStockPrice)
+actualStockPrice = scaler.inverse_transform(y_test)
+
+plotData = nikeDataDF.iloc[trainingDataSize:, 1:2]
+plotData['predictedStockPrice'] = predictedStockPrice
+
+plt.plot(plotData.iloc[:, 0], color='red', label='Actual Nike Stock Price') #this first plot should get actual nike stock price
+plt.plot(plotData['predictedStockPrice'], color='blue', label='Predicted Nike Stock Price')
+plt.title('Nike Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Nike Stock Price')
+plt.legend()
+plt.show()
+
+
+
+### TODO ####
+""" in the plot data add another row next to the last one that will be the price prediction for tomorrow. You need to get index of last column, convert to date and add a day to it """
