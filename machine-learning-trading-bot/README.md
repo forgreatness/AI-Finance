8124b44b86296da81165b4a6a38dafd1e28b2790
Danh Nguyen2 <Danh_Nguyen2@edwards.com>
2024-07-05 19:46:49 -0700

    created a baseline model that will take signal which are prothetically created which means whether price today is up or down 1 for up -1 for down - using this signal and sma of 50 and sma of 200 we will predict the actual price of whether it went up or down. Then compare performance

--- machine-learning-trading-bot/README.md
+++ machine-learning-trading-bot/README.md
@@ -1,2 +1,16 @@
 ## Purpose
 The goals of this project is to create a trading bot, that will utilize machine learning to help confirm a condition for when to place a trade. The trading bot should have backtesting to see its performance, before making any real life trade. As it make trades in real life, it should have limitation place on position sizing, stop loss. 
+
+
+Machine Learning Trading Bot (by ML_Trading_Bot)
+Algorithm Review (Step by Step)
+- Takes only the close column among all data available from dataset
+- Create an actual returns column by taking the close column and compare it previous day
+- Create a short window and a long window 50 and 200 create 2 more column equalling 4 with short window being taking the last 50 close value to compute mean and do same for long window of 200
+- Create signal column totalling 5, this signal column is going to be 1 if actual return for that day is greater than 1
+- Create another column totalling 6 called Strategy Returns by using the signal value of previous day (whether previous day was up) and times it by the actual returns for current day. If yesterday is up then yesterday time today could be negative if return is negative (positive if its positive)
+plot the value of strategy returns using cumulative return to plot the return of strategy returns over time. 
+- Create x and y representing feature inputs and output of the model. features will be sma_short and sma_long. output is y which is signal
+- Create 2 time frame trainingbegin and trainingend 
+- Filter x using the timeframe store value in x_train, filter y as well in y_train
+do same for x_test and y_test
--- machine-learning-trading-bot/main.py
+++ machine-learning-trading-bot/main.py
@@ -17,7 +17,7 @@ from dateutil.relativedelta import relativedelta
 stockDataFileName = 'jpm-stock-data.csv'
 ticker = 'jpm'
 todayDate = datetime.now()
-dataStartDate = todayDate - relativedelta(years=3)
+dataStartDate = todayDate - relativedelta(years=10)
 
 if os.path.exists(stockDataFileName):
     stockData = pd.read_csv(stockDataFileName, index_col=0, parse_dates=True)
@@ -29,5 +29,75 @@ else:
         print(f"An error occured fetching stock data: {e}")
 
 
-stockPricesOnly = stockData.loc[:, ['Close']]
-print(stockPricesOnly)
+processedStockData = stockData.loc[:, ['Close']]
+processedStockData['Actual Returns'] = processedStockData['Close'].pct_change()
+processedStockData = processedStockData.dropna()
+
+# Generate SMA trading signal
+short_window = 50
+long_window = 200
+
+processedStockData['SMA_short'] = processedStockData['Close'].rolling(window=short_window).mean()
+processedStockData['SMA_long'] = processedStockData['Close'].rolling(window=long_window).mean()
+processedStockData = processedStockData.dropna()
+
+# initialize signal for each stock price instance
+processedStockData['Signal'] = 0.0
+processedStockData.loc[(processedStockData['Actual Returns'] >= 0), 'Signal'] = 1
+processedStockData.loc[(processedStockData['Actual Returns'] < 0), 'Signal'] = -1
+
+# Actual Returns is whether today price was higher than yesterday in percentage (+/-)
+# Signal is whether today price was higher than yesterday in (true false (1/0))
+# shifting means we are looking at actual returns of yesterday compare to day before, it its up then the strategy return is 
+# essential today return times signal from yesterday
+processedStockData['Strategy Returns'] = processedStockData['Actual Returns'] * processedStockData['Signal'].shift()
+
+# I need to plot the Strategy Returns column so we can see the new column we created
+plt.figure(figsize=(10, 6))
+# Take the strategy returns which are yesterday buy or not 
+plt.plot(processedStockData.index, (1 + processedStockData['Strategy Returns']).cumprod(), label='Strategy Returns', color='blue')
+plt.xlabel('Date')
+plt.ylabel('Strategy Returns')
+plt.title('Strategy Returns Over Time')
+
+plt.legend()
+plt.grid(True)
+# plt.show()
+
+
+x = processedStockData[['SMA_short', 'SMA_long']].shift().dropna() #takes the sma_short and sma_long and put it in a new dataframe
+y = processedStockData['Signal'] #actual values are the signal which means if today close price compare to yesterday is greater or less
+
+training_begin = x.index.min()
+training_end = x.index.min() + DateOffset(months=6)
+
+x_train = x.loc[training_begin:training_end]
+y_train = y.loc[training_begin:training_end]
+x_test = x.loc[training_end+DateOffset(hours=1):]
+y_test = y.loc[training_end+DateOffset(hours=1):]
+
+scaler = StandardScaler()
+x_scaler = scaler.fit(x_train)
+x_train_scaled = x_scaler.transform(x_train)
+x_test_scaled = x_scaler.transform(x_test)
+
+# Choose the model to train and fit data
+svmModel = svm.SVC()
+svmModel = svmModel.fit(x_train_scaled, y_train)
+svmPred = svmModel.predict(x_test_scaled)
+
+svm_testing_report = classification_report(y_test, svmPred)
+print("Below is the testing report for the SVM model: \n", svm_testing_report)
+
+predictionsDF = pd.DataFrame(index=x_test.index)
+predictionsDF['Predicted'] = svmPred
+predictionsDF['Actual Returns'] = processedStockData['Actual Returns']
+predictionsDF['Strategy Returns'] = processedStockData['Actual Returns'] * predictionsDF['Predicted']
+
+print(predictionsDF)
+
+plt.plot(predictionsDF.index, (1 + predictionsDF[['Actual Returns', 'Strategy Returns']]).cumprod())
+plt.show()
+
+
+
